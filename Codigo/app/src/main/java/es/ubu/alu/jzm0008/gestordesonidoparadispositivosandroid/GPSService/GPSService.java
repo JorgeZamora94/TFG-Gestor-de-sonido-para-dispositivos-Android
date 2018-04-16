c28378a30372d1d05bffe264f42fffd49f6f1578
Jorge Zamora <cerbonero@hotmail.com>
2018-04-16 18:37:44 +0200

    Utilizar servicios para la obtención de la localización GPS

---
+++ Codigo/app/src/main/java/es/ubu/alu/jzm0008/gestordesonidoparadispositivosandroid/GPSService/GPSService.java
@@ -0,0 +1,54 @@
+package es.ubu.alu.jzm0008.gestordesonidoparadispositivosandroid.GPSService;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.widget.Toast;
+
+import es.ubu.alu.jzm0008.gestordesonidoparadispositivosandroid.GPSListener.GPSListener;
+
+public class GPSService extends IntentService {
+
+    public GPSService() {
+        super("GPSService");
+    }
+
+    public static void startGetGPS(Context context) {
+        Intent intent = new Intent(context, GPSService.class);
+        context.startService(intent);
+
+        GPSListener gpsListener = new GPSListener(context);
+        System.out.println("Service");
+        System.out.println(gpsListener.getLocation());
+
+        if(gpsListener.getLocation()!=null){
+            Toast.makeText(context,gpsListener.getLocation().toString(),Toast.LENGTH_SHORT).show();
+        } else
+        {
+            Toast.makeText(context,"nada",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void startHiloGPS(final Context context) {
+        final GPSListener gpsListener = new GPSListener(context);
+        new Thread(new Runnable() {
+
+            public void run() {
+                while(true) {
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println(gpsListener.getLocation());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+}
---
+++ Codigo/app/src/main/java/es/ubu/alu/jzm0008/gestordesonidoparadispositivosandroid/GPSService/ServiceActivity.java
@@ -0,0 +1,18 @@
+package es.ubu.alu.jzm0008.gestordesonidoparadispositivosandroid.GPSService;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import es.ubu.alu.jzm0008.gestordesonidoparadispositivosandroid.R;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+        GPSService s = new GPSService();
+        s.startHiloGPS(this);
+    }
+}
